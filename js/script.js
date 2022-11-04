51b0f9f6767271b2c386bbfb38bf36c17c1e0066
isma-91 <isma91@hotmail.it>
2022-11-04 18:42:23 +0100

    Fix some sintax

--- js/script.js
+++ js/script.js
@@ -10,23 +10,18 @@
   //- Con il math.random creare 5 numeri casuali da 1 a 100 che non si ripetono
   //- Pushare i numeri random non uguali nell'array dei numeri random
   //- Stampare la lista dei numeri random generati nella pagina
-  - Creare un setTimeout che dopo 30 secondi faccia nascondere i numeri random. 
-  - Chiedere all'utente tramite il prompt dei comandi quali erano i 5 numeri
-  - Creare un array dove andranno pushati i numeri che ci darà l'utente
+  //- Creare un setTimeout che dopo 30 secondi faccia nascondere i numeri random. 
+  //- Chiedere all'utente tramite il prompt dei comandi quali erano i 5 numeri
+  //- Creare un array dove andranno pushati i numeri che ci darà l'utente
   - Inseriti tutti e 5 i numeri da parte dell'utente confrontare quali e quanti numeri ha ricordato (confrontando i 2 array?)
 */
 const arrRandomNumbers = [];
 const arrUserNumbers = [];
-const eleUSer = document.querySelector(".user-numbers");
+const eleUser = document.querySelector(".user-numbers");
 const eleListNumber = document.querySelector(".n-random");
 const eleVerify = document.querySelector(".verify");
 const eleForm = document.querySelector("form");
 // const eleBtn = document.querySelector(".btn");
-// let eleN1 = document.querySelector("#n1");
-// let eleN2 = document.querySelector("#n2");
-// let eleN3 = document.querySelector("#n3");
-// let eleN4 = document.querySelector("#n4");
-// let eleN5 = document.querySelector("#n5");
 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -35,7 +30,7 @@ function getRandomInteger(min, max) {
 //Si può "modificare leggermente per renderla universale e poterla utilizzare tutte le volte che devo dare e togliere hidden a un elemento forse..."
 function hidden() {
   eleListNumber.classList.add("hidden");
-  eleUSer.classList.remove("hidden");
+  eleUser.classList.remove("hidden");
   // parseInt(prompt("Quali erano i 5 numeri che erano a schermo?"));
 }
 
@@ -88,6 +83,6 @@ eleForm.addEventListener("submit", function (event) {
   } else {
     eleVerify.innerHTML += "Hai perso!";
   }
-  eleUSer.classList.add("hidden");
+  eleUser.classList.add("hidden");
   eleVerify.classList.remove("hidden");
 });
